b93eb44d45eeab2e513530ad3a8853adee4c9326
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2019-11-03 22:00:47 -0300

    COMMIT 3

--- Dollar.java
+++ Dollar.java
@@ -5,13 +5,8 @@ public class Dollar extends Money
 		this.amount = amount;
 	}
 
-	Dollar times(int multiplier) 
+	Money times(int multiplier) 
     {
 		return new Dollar(amount * multiplier);
-    }
-    public boolean equals(Object object)  
-    {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
